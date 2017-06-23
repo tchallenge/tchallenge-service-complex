72fb1aea48148b3445c791b8d2cc183b50f28270
onlythenaive <ilya.gubarev@gmail.com>
2017-06-23 14:19:55 +0300

    routing conventions added

---
+++ sources/src/main/java/ru/tchallenge/service/complex/convention/routing/RouteGet.java
@@ -0,0 +1,23 @@
+package ru.tchallenge.service.complex.convention.routing;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@RequestMapping(method = RequestMethod.GET)
+public @interface RouteGet {
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String[] value() default {};
+}
