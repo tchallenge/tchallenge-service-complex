218e10f0f92a381054f55e8458b9d4bb8685f8cf
onlythenaive <ilya.gubarev@gmail.com>
2017-06-23 15:38:31 +0300

    facade changed to class, simple authentication implementation added

--- sources/src/main/groovy/ru/tchallenge/service/complex/behavior/component/Facade.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/behavior/component/Facade.groovy
@@ -2,7 +2,21 @@ package ru.tchallenge.service.complex.behavior.component
 
 import groovy.transform.CompileStatic
 
-@CompileStatic
-trait Facade {
+import org.springframework.beans.factory.annotation.Autowired
 
+import ru.tchallenge.service.complex.security.authentication.AuthenticationContext
+
+@CompileStatic
+abstract class Facade {
+
+    @Autowired
+    protected AuthenticationContext authenticationContext
+
+    protected def authenticated() {
+        def authentication = authenticationContext.authentication
+        if (authentication == null) {
+            throw new RuntimeException("not authenticated")
+        }
+        return authentication
+    }
 }
