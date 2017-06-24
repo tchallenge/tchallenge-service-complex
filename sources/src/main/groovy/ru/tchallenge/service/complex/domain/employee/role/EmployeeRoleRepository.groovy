45baf9fdb081cdfc795acc41692399e288fa4a28
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 14:34:52 +0300

    Employee Role added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/employee/role/EmployeeRoleRepository.groovy
@@ -0,0 +1,10 @@
+package ru.tchallenge.service.complex.domain.employee.role
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedRepository
+
+@CompileStatic
+interface EmployeeRoleRepository extends GenericEnumeratedRepository<EmployeeRole> {
+
+}
