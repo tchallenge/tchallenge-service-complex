3c0425d40eb7451a9f0dcfde89c3436dc2b2e5e1
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 20:05:13 +0300

    Account repository fixed to use Pageable from the correct package

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/AccountRepository.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/AccountRepository.groovy
@@ -1,10 +1,9 @@
 package ru.tchallenge.service.complex.domain.account
 
-import java.awt.print.Pageable
-
 import groovy.transform.CompileStatic
 
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 
 import ru.tchallenge.service.complex.common.ordinal.GenericOrdinalRepository
