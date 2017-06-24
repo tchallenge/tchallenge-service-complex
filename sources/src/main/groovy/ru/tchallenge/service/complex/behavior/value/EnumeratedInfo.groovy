024ea5b7f8714065a42268da942982e618058f7b
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 07:37:47 +0300

    enumerated values added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/behavior/value/EnumeratedInfo.groovy
@@ -0,0 +1,11 @@
+package ru.tchallenge.service.complex.behavior.value
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class EnumeratedInfo extends InfoValue {
+
+    String textcode
+    String title
+    String description
+}
---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/behavior/value/EnumeratedInvoice.groovy
@@ -0,0 +1,9 @@
+package ru.tchallenge.service.complex.behavior.value
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class EnumeratedInvoice extends InvoiceValue {
+
+    String textcode
+}
