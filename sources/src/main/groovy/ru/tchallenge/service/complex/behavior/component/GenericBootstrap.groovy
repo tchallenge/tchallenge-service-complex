9e2f008f4537b9a46e4d97d1f59775e5e409b1a8
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 06:36:21 +0300

    generic bootstrap marked by CompileStatic

--- sources/src/main/groovy/ru/tchallenge/service/complex/behavior/component/GenericBootstrap.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/behavior/component/GenericBootstrap.groovy
@@ -2,10 +2,13 @@ package ru.tchallenge.service.complex.behavior.component
 
 import javax.annotation.PostConstruct
 
+import groovy.transform.CompileStatic
+
 import org.springframework.beans.factory.annotation.Autowired
 
 import ru.tchallenge.service.complex.behavior.entity.GenericEntity
 
+@CompileStatic
 abstract class GenericBootstrap<E extends GenericEntity, ID extends Serializable> {
 
     @Autowired
@@ -13,7 +16,7 @@ abstract class GenericBootstrap<E extends GenericEntity, ID extends Serializable
 
     @PostConstruct
     protected void bootstrap() {
-        entities().forEach { repository.save(it) }
+        entities().forEach { E entity -> repository.save(entity) }
     }
 
     protected abstract Collection<E> entities()
