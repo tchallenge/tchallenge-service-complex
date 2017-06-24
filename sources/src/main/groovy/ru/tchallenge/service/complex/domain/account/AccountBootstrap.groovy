5a5316c64e6aa4f7e74514f687add8746250a068
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 21:02:44 +0300

    Account bootstrap depends on enumerated bootstraps

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/AccountBootstrap.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/AccountBootstrap.groovy
@@ -6,18 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired
 
 import ru.tchallenge.service.complex.common.enumerated.EnumeratedHelper
 import ru.tchallenge.service.complex.common.ordinal.GenericOrdinalBootstrap
+import ru.tchallenge.service.complex.common.ordinal.sequence.OrdinalSequenceBootstrap
 import ru.tchallenge.service.complex.common.ordinal.sequence.OrdinalSequenceService
 import ru.tchallenge.service.complex.convention.component.BootstrapComponent
+import ru.tchallenge.service.complex.domain.account.realm.AccountRealmBootstrap
 import ru.tchallenge.service.complex.domain.account.realm.AccountRealmRepository
+import ru.tchallenge.service.complex.domain.account.status.AccountStatusBootstrap
 import ru.tchallenge.service.complex.domain.account.status.AccountStatusRepository
+import ru.tchallenge.service.complex.domain.account.verification.AccountVerificationBootstrap
 import ru.tchallenge.service.complex.domain.account.verification.AccountVerificationRepository
 import ru.tchallenge.service.complex.domain.employee.Employee
+import ru.tchallenge.service.complex.domain.employee.role.EmployeeRoleBootstrap
 import ru.tchallenge.service.complex.domain.employee.role.EmployeeRoleRepository
 import ru.tchallenge.service.complex.domain.person.Person
+import ru.tchallenge.service.complex.domain.robot.role.RobotRoleBootstrap
 
 @CompileStatic
 @BootstrapComponent
 class AccountBootstrap extends GenericOrdinalBootstrap<Account> {
+
+    @Autowired
+    protected OrdinalSequenceBootstrap ordinalSequenceBootstrap
+
+    @Autowired
+    protected AccountRealmBootstrap accountRealmBootstrap
+
+    @Autowired
+    protected AccountStatusBootstrap accountStatusBootstrap
+
+    @Autowired
+    protected AccountVerificationBootstrap accountVerificationBootstrap
+
+    @Autowired
+    protected EmployeeRoleBootstrap employeeRoleBootstrap
+
+    @Autowired
+    protected RobotRoleBootstrap robotRoleBootstrap
 
     @Autowired
     protected AccountRealmRepository accountRealmRepository
