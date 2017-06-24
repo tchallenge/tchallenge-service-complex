fc80da8ca4c1fffe5dbe6847ab291e2d58e6ba73
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 08:37:09 +0300

    Account Status entity added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/status/AccountStatusBootstrap.groovy
@@ -0,0 +1,16 @@
+package ru.tchallenge.service.complex.domain.account.status
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedBootstrap
+import ru.tchallenge.service.complex.convention.component.BootstrapComponent
+
+@CompileStatic
+@BootstrapComponent
+class AccountStatusBootstrap extends GenericEnumeratedBootstrap<AccountStatus> {
+
+    @Override
+    protected Collection<AccountStatus> enumeratedEntities() {
+        return []
+    }
+}
