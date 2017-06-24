a4bb84c0710609fcde2462c2abb4c8a8a180cce7
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 17:19:27 +0300

    Robot attributes updated

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/robot/Robot.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/robot/Robot.groovy
@@ -1,7 +1,10 @@
 package ru.tchallenge.service.complex.domain.robot
 
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
 import javax.persistence.OneToOne
 import javax.persistence.Table
 
@@ -9,6 +12,7 @@ import groovy.transform.CompileStatic
 
 import ru.tchallenge.service.complex.common.ordinal.GenericSecondaryEntity
 import ru.tchallenge.service.complex.domain.account.Account
+import ru.tchallenge.service.complex.domain.robot.role.RobotRole
 
 @CompileStatic
 @Entity
@@ -18,4 +22,14 @@ class Robot extends GenericSecondaryEntity {
     @OneToOne
     @JoinColumn(name = "account_id")
     private Account account
+
+    @Column(name = "title")
+    String title
+
+    @Column(name = "description")
+    String description
+
+    @ManyToMany
+    @JoinTable(name = "robot_role_to_robot_map", joinColumns = @JoinColumn(name = "robot_id"))
+    Collection<RobotRole> roles
 }
