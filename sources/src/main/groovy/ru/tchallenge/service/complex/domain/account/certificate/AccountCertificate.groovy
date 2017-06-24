f150b00d5194f29a83fcd8eb13fb12cec70207fb
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 15:12:45 +0300

    Account Certificate added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/certificate/AccountCertificate.groovy
@@ -0,0 +1,41 @@
+package ru.tchallenge.service.complex.domain.account.certificate
+
+import java.time.Instant
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.complementary.GenericComplementaryEntity
+
+@CompileStatic
+@Entity
+@Table(name = "account_certificate")
+class AccountCertificate extends GenericComplementaryEntity {
+
+    @Column
+    Integer active
+
+    @Column
+    String hash
+
+    @Column
+    private Instant createdAt
+
+    @Column
+    private Instant expiresAt
+
+    Instant getCreatedAt() {
+        return createdAt
+    }
+
+    Instant getExpiresAt() {
+        return expiresAt
+    }
+
+    @Override
+    protected void onInsert() {
+        createdAt = Instant.now()
+    }
+}
