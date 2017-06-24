d57dd6764a7797adc0c7b18e070cb4967a58097f
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 17:28:52 +0300

    Employee attributes updated

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/employee/Employee.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/employee/Employee.groovy
@@ -2,6 +2,8 @@ package ru.tchallenge.service.complex.domain.employee
 
 import javax.persistence.Entity
 import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
 import javax.persistence.OneToOne
 import javax.persistence.Table
 
@@ -9,6 +11,7 @@ import groovy.transform.CompileStatic
 
 import ru.tchallenge.service.complex.common.ordinal.GenericSecondaryEntity
 import ru.tchallenge.service.complex.domain.account.Account
+import ru.tchallenge.service.complex.domain.employee.role.EmployeeRole
 
 @CompileStatic
 @Entity
@@ -18,4 +21,8 @@ class Employee extends GenericSecondaryEntity {
     @OneToOne
     @JoinColumn(name = "account_id")
     private Account account
+
+    @ManyToMany
+    @JoinTable(name = "employee_role_to_employee_map", joinColumns = @JoinColumn(name = "employee_id"))
+    Collection<EmployeeRole> roles
 }
