98c6829fb3aea6203ed73db4b4b2e7fb17b64937
onlythenaive <ilya.gubarev@gmail.com>
2017-06-24 14:51:35 +0300

    Robot Role added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/robot/role/RobotRoleBootstrap.groovy
@@ -0,0 +1,33 @@
+package ru.tchallenge.service.complex.domain.robot.role
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedBootstrap
+import ru.tchallenge.service.complex.convention.component.BootstrapComponent
+
+@CompileStatic
+@BootstrapComponent
+class RobotRoleBootstrap extends GenericEnumeratedBootstrap<RobotRole> {
+
+    @Override
+    protected Collection<RobotRole> enumeratedEntities() {
+        return [
+                new RobotRole(
+                        textcode: "CANDVIEW",
+                        title: "Просмотрщик кандидатов"
+                ),
+                new RobotRole(
+                        textcode: "EVENTVIEW",
+                        title: "Просмотрщик событий"
+                ),
+                new RobotRole(
+                        textcode: "TASKVIEW",
+                        title: "Просмотрщик задач"
+                ),
+                new RobotRole(
+                        textcode: "WBKVIEW",
+                        title: "Просмотрщик рабочих тетрадей"
+                )
+        ]
+    }
+}
---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/robot/role/RobotRoleRepository.groovy
@@ -0,0 +1,10 @@
+package ru.tchallenge.service.complex.domain.robot.role
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedRepository
+
+@CompileStatic
+interface RobotRoleRepository extends GenericEnumeratedRepository<RobotRole> {
+
+}
