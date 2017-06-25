193cd81df83de50305cd1d693abd139733d71db9
onlythenaive <ilya.gubarev@gmail.com>
2017-06-26 00:12:38 +0300

    base authentication methods added

--- sources/src/main/groovy/ru/tchallenge/service/complex/common/GenericFacade.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/common/GenericFacade.groovy
@@ -4,6 +4,8 @@ import groovy.transform.CompileStatic
 
 import org.springframework.beans.factory.annotation.Autowired
 
+import ru.tchallenge.service.complex.common.enumerated.EnumeratedInfo
+import ru.tchallenge.service.complex.domain.account.AccountInfo
 import ru.tchallenge.service.complex.security.authentication.AuthenticationContext
 
 @CompileStatic
@@ -12,11 +14,33 @@ abstract class GenericFacade extends GenericComponent {
     @Autowired
     protected AuthenticationContext authenticationContext
 
-    protected def authenticated() {
-        def authentication = authenticationContext.authentication
-        if (authentication == null) {
-            throw new RuntimeException("not authenticated")
+    protected AccountInfo authenticated() {
+        def result = authenticationContext.authentication.account
+        if (!result) {
+            throw unauthorized()
         }
-        return authentication
+        return result
+    }
+
+    protected boolean authenticatedEmployee(String... roles) {
+        def result = authenticated()
+        def employee = result.employee
+        if (!employee) {
+            return false
+        }
+        if (!roles || !roles.size()) {
+            return true
+        }
+        for (EnumeratedInfo employeeRole : employee.roles) {
+            def textcode = employeeRole.textcode
+            if (textcode == "ADMIN" || roles.contains(textcode)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    protected RuntimeException unauthorized() {
+        throw new RuntimeException("unauthorized")
     }
 }
