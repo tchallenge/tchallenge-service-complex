842cc9d7f914943f3d03325372b2172cc702e689
onlythenaive <ilya.gubarev@gmail.com>
2017-06-25 20:34:24 +0300

    web configurator added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/WebConfigurator.groovy
@@ -0,0 +1,63 @@
+package ru.tchallenge.service.complex
+
+import groovy.transform.CompileStatic
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+
+@CompileStatic
+@Configuration
+@EnableWebMvc
+class WebConfigurator extends WebMvcConfigurerAdapter {
+
+    @Bean
+    FilterRegistrationBean corsFilter() {
+        def source = new UrlBasedCorsConfigurationSource()
+        def config = new CorsConfiguration()
+        config.setAllowCredentials(true)
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("OPTIONS")
+        config.addAllowedMethod("HEAD")
+        config.addAllowedMethod("GET")
+        config.addAllowedMethod("PUT")
+        config.addAllowedMethod("POST")
+        config.addAllowedMethod("DELETE")
+        config.addAllowedMethod("PATCH")
+        source.registerCorsConfiguration("/**", config)
+        def bean = new FilterRegistrationBean(new CorsFilter(source))
+        bean.setOrder(0)
+        return bean
+    }
+
+    @Override
+    void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false)
+    }
+
+    @Override
+    void configureMessageConverters(List<HttpMessageConverter<?>> list) {
+        list.add(new MappingJackson2HttpMessageConverter(jackson2ObjectMapper()))
+    }
+
+    private static ObjectMapper jackson2ObjectMapper() {
+        return new Jackson2ObjectMapperBuilder()
+                .failOnEmptyBeans(false)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+                .build()
+    }
+}
