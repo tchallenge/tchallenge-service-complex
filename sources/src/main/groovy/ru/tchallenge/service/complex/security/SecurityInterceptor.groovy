b6fa6adbaf17717b4162538ea3cc6f8992f4a4ba
onlythenaive <ilya.gubarev@gmail.com>
2017-06-25 22:59:25 +0300

    security interceptor implementation updated

--- sources/src/main/groovy/ru/tchallenge/service/complex/security/SecurityInterceptor.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/security/SecurityInterceptor.groovy
@@ -4,15 +4,34 @@ import javax.servlet.http.HttpServletRequest
 
 import groovy.transform.CompileStatic
 
+import org.springframework.beans.factory.annotation.Autowired
+
 import ru.tchallenge.service.complex.common.GenericInterceptor
 import ru.tchallenge.service.complex.convention.component.InterceptorComponent
+import ru.tchallenge.service.complex.security.authentication.AuthenticationContext
+import ru.tchallenge.service.complex.security.authentication.AuthenticationService
 
 @CompileStatic
 @InterceptorComponent
 class SecurityInterceptor extends GenericInterceptor {
 
+    @Autowired
+    protected AuthenticationContext authenticationContext
+
+    @Autowired
+    protected AuthenticationService authenticationService
+
     @Override
     boolean preHandle(HttpServletRequest request) {
-        return true
+        if (request.requestURI == "/authentication") {
+            return true
+        }
+        def tokenPayload = request.getHeader("tchallenge-security-token-payload")
+        if (tokenPayload) {
+            def authentication = authenticationService.createFromTokenPayload(tokenPayload)
+            authenticationContext.setAuthentication(authentication)
+            return true
+        }
+        throw new RuntimeException("unauthenticated")
     }
 }
