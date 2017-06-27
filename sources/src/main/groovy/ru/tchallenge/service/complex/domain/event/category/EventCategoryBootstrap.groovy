5c35279f413ae91c20af813bf71bfc2bad8222c0
onlythenaive <ilya.gubarev@gmail.com>
2017-06-28 00:34:28 +0300

    Event Category added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/category/EventCategoryBootstrap.groovy
@@ -0,0 +1,29 @@
+package ru.tchallenge.service.complex.domain.event.category
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedBootstrap
+import ru.tchallenge.service.complex.convention.component.BootstrapComponent
+
+@CompileStatic
+@BootstrapComponent
+class EventCategoryBootstrap extends GenericEnumeratedBootstrap<EventCategory> {
+
+    @Override
+    protected Collection<EventCategory> enumeratedEntities() {
+        return [
+                new EventCategory(
+                        textcode: "FORUM",
+                        title: "Конференеция, публичное мероприятие"
+                ),
+                new EventCategory(
+                        textcode: "INTERVIEW",
+                        title: "Предварительный тест для интервью"
+                ),
+                new EventCategory(
+                        textcode: "SCREENING",
+                        title: "Скриннинг, вступительное задание"
+                )
+        ]
+    }
+}
---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/category/EventCategoryRepository.groovy
@@ -0,0 +1,10 @@
+package ru.tchallenge.service.complex.domain.event.category
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedRepository
+
+@CompileStatic
+interface EventCategoryRepository extends GenericEnumeratedRepository<EventCategory> {
+
+}
