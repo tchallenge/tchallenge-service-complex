4b66684814dcae0d218fe085c8fa60a41586b18e
onlythenaive <ilya.gubarev@gmail.com>
2017-06-28 00:48:33 +0300

    Event added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/Event.groovy
@@ -0,0 +1,46 @@
+package ru.tchallenge.service.complex.domain.event
+
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.ordinal.GenericOrdinalEntity
+import ru.tchallenge.service.complex.common.timestamp.TimestampedEntity
+import ru.tchallenge.service.complex.domain.event.category.EventCategory
+import ru.tchallenge.service.complex.domain.event.interval.EventInterval
+import ru.tchallenge.service.complex.domain.event.status.EventStatus
+
+@CompileStatic
+@Entity
+@Table(name = "event")
+class Event extends GenericOrdinalEntity implements TimestampedEntity {
+
+    @Column(name = "textcode")
+    String textcode
+
+    @Column(name = "title")
+    String title
+
+    @Column(name = "description")
+    String description
+
+    @Column(name = "greeting")
+    String greeting
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    Collection<EventInterval> intervals = []
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    EventCategory category
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    EventStatus status
+}
--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/interval/EventInterval.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/interval/EventInterval.groovy
@@ -3,16 +3,23 @@ package ru.tchallenge.service.complex.domain.event.interval
 import java.time.Instant
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.Table
 
 import groovy.transform.CompileStatic
 
 import ru.tchallenge.service.complex.common.complementary.GenericComplementaryEntity
+import ru.tchallenge.service.complex.domain.event.Event
 
 @CompileStatic
 @Entity
 @Table(name = "event_interval")
 class EventInterval extends GenericComplementaryEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    Event event
 
     @Column
     Instant since
