90f6d60600f38a54045f855cd0b3cd8b4fc06711
onlythenaive <ilya.gubarev@gmail.com>
2017-06-29 00:05:25 +0300

    Event router update to use real facade

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventRouter.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventRouter.groovy
@@ -2,41 +2,53 @@ package ru.tchallenge.service.complex.domain.event
 
 import groovy.transform.CompileStatic
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 
 import ru.tchallenge.service.complex.common.GenericRouter
-import ru.tchallenge.service.complex.common.search.GenericSearchInvoice
+import ru.tchallenge.service.complex.common.search.SearchInfo
 import ru.tchallenge.service.complex.convention.component.RouterComponent
 import ru.tchallenge.service.complex.convention.routing.RouteGet
+import ru.tchallenge.service.complex.convention.routing.RoutePatch
 import ru.tchallenge.service.complex.convention.routing.RoutePost
-import ru.tchallenge.service.complex.convention.routing.RoutePut
+import ru.tchallenge.service.complex.convention.security.NoAuthentication
 
 @CompileStatic
 @RouterComponent("/events")
 class EventRouter extends GenericRouter {
 
+    @Autowired
+    protected EventFacade eventFacade
+
     @RoutePost
-    def create(@RequestBody EventInvoice invoice) {
-        return new EventInfo(textcode: invoice.textcode)
+    EventInfo create(@RequestBody EventInvoice invoice) {
+        return eventFacade.create(invoice)
     }
 
+    @NoAuthentication
     @RouteGet("/{textcode}")
-    def getByTextcode(@PathVariable("textcode") String textcode) {
-        return new EventInfo(textcode: textcode)
+    EventInfo getByTextcode(@PathVariable("textcode") String textcode) {
+        return eventFacade.get(textcode)
     }
 
     @RouteGet
-    def search(EventInvoice invoice) {
-        return [
-                new EventInfo(),
-                new EventInfo(),
-                new EventInfo()
-        ]
+    SearchInfo<EventInfo> search(EventSearchInvoice invoice) {
+        return eventFacade.search(ensureOrderingAndPaging(invoice))
     }
 
-    @RoutePut
-    def update(@RequestBody EventInvoice invoice) {
-        return new EventInfo(textcode: invoice.textcode)
+    @RoutePatch
+    EventInfo update(@RequestBody EventInvoice invoice) {
+        return eventFacade.update(invoice)
+    }
+
+    private static EventSearchInvoice ensureOrderingAndPaging(EventSearchInvoice invoice) {
+        return invoice.with {
+            orderDescending = invoice.orderDescending ?: false
+            orderProperties = invoice.orderProperties ?: [] as Collection<String>
+            pageNumber = invoice.pageNumber ?: 1
+            pageSize = invoice.pageSize ?: 10
+            it
+        }
     }
 }
