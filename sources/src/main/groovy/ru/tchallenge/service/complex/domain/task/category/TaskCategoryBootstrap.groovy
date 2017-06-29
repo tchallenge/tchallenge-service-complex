318282ae1a598d20417caef5c7979efc07ea51b5
onlythenaive <ilya.gubarev@gmail.com>
2017-06-29 06:02:30 +0300

    Task Category added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/task/category/TaskCategoryBootstrap.groovy
@@ -0,0 +1,41 @@
+package ru.tchallenge.service.complex.domain.task.category
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedBootstrap
+import ru.tchallenge.service.complex.convention.component.BootstrapComponent
+
+@CompileStatic
+@BootstrapComponent
+class TaskCategoryBootstrap extends GenericEnumeratedBootstrap<TaskCategory> {
+
+    @Override
+    protected Collection<TaskCategory> enumeratedEntities() {
+        return [
+                new TaskCategory(
+                        textcode: "COMMON",
+                        title: "Задачи на общую логику"
+                ),
+                new TaskCategory(
+                        textcode: "OOD",
+                        title: "Объекто-ориентированный дизайн"
+                ),
+                new TaskCategory(
+                        textcode: "JAVA",
+                        title: "Java и JVM"
+                ),
+                new TaskCategory(
+                        textcode: "JAVASCRIPT",
+                        title: "JavaScript и ECMA"
+                ),
+                new TaskCategory(
+                        textcode: "TEST",
+                        title: "Тестирование ПО"
+                ),
+                new TaskCategory(
+                        textcode: "SQL",
+                        title: "SQL и реляционная модель"
+                )
+        ]
+    }
+}
---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/task/category/TaskCategoryRepository.groovy
@@ -0,0 +1,10 @@
+package ru.tchallenge.service.complex.domain.task.category
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedRepository
+
+@CompileStatic
+interface TaskCategoryRepository extends GenericEnumeratedRepository<TaskCategory> {
+
+}
