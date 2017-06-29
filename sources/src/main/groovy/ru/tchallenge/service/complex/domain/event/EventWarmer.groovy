12787b328ccab302b8e9a2a9467d497d48d46f08
onlythenaive <ilya.gubarev@gmail.com>
2017-06-29 05:51:38 +0300

    Event warmer added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventWarmer.groovy
@@ -0,0 +1,24 @@
+package ru.tchallenge.service.complex.domain.event
+
+import groovy.transform.CompileStatic
+
+import org.springframework.beans.factory.annotation.Autowired
+
+import ru.tchallenge.service.complex.common.GenericWarmer
+import ru.tchallenge.service.complex.convention.component.WarmerComponent
+
+@CompileStatic
+@WarmerComponent
+class EventWarmer extends GenericWarmer {
+
+    @Autowired
+    protected EventService eventService
+
+    @Override
+    void run() {
+        eventService.search(new EventSearchInvoice(
+                pageNumber: 1,
+                pageSize: 1000
+        ))
+    }
+}
