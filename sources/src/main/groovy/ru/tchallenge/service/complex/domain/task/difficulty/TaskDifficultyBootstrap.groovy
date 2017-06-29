e51cb9b9189847381064bbbe3172546d4e7bbe7f
onlythenaive <ilya.gubarev@gmail.com>
2017-06-29 06:07:23 +0300

    Task Difficulty added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/task/difficulty/TaskDifficultyBootstrap.groovy
@@ -0,0 +1,37 @@
+package ru.tchallenge.service.complex.domain.task.difficulty
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedBootstrap
+import ru.tchallenge.service.complex.convention.component.BootstrapComponent
+
+@CompileStatic
+@BootstrapComponent
+class TaskDifficultyBootstrap extends GenericEnumeratedBootstrap<TaskDifficulty> {
+
+    @Override
+    protected Collection<TaskDifficulty> enumeratedEntities() {
+        return [
+                new TaskDifficulty(
+                        textcode: "ELEMENTARY",
+                        title: "Простейшие задачи"
+                ),
+                new TaskDifficulty(
+                        textcode: "EASY",
+                        title: "Относительно легкие"
+                ),
+                new TaskDifficulty(
+                        textcode: "MEDIUM",
+                        title: "Умеренно сложные"
+                ),
+                new TaskDifficulty(
+                        textcode: "HARD",
+                        title: "Сложные"
+                ),
+                new TaskDifficulty(
+                        textcode: "ULTIMATE",
+                        title: "Задачи повышенной сложности"
+                )
+        ]
+    }
+}
---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/task/difficulty/TaskDifficultyRepository.groovy
@@ -0,0 +1,10 @@
+package ru.tchallenge.service.complex.domain.task.difficulty
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedRepository
+
+@CompileStatic
+interface TaskDifficultyRepository extends GenericEnumeratedRepository<TaskDifficulty> {
+
+}
