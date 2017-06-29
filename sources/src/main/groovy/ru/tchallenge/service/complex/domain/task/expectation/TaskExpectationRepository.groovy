78b25ef4f0180cf75c829ae31e0cc1cf7991d6b2
onlythenaive <ilya.gubarev@gmail.com>
2017-06-29 06:50:01 +0300

    Task Expectation

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/task/expectation/TaskExpectationRepository.groovy
@@ -0,0 +1,10 @@
+package ru.tchallenge.service.complex.domain.task.expectation
+
+import groovy.transform.CompileStatic
+
+import ru.tchallenge.service.complex.common.enumerated.GenericEnumeratedRepository
+
+@CompileStatic
+interface TaskExpectationRepository extends GenericEnumeratedRepository<TaskExpectation> {
+
+}
