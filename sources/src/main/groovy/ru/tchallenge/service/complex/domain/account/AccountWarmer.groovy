484b649fed382c87686b4d34b924d941f09716dd
onlythenaive <ilya.gubarev@gmail.com>
2017-06-29 05:51:26 +0300

    Account warmer added

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/account/AccountWarmer.groovy
@@ -0,0 +1,24 @@
+package ru.tchallenge.service.complex.domain.account
+
+import groovy.transform.CompileStatic
+
+import org.springframework.beans.factory.annotation.Autowired
+
+import ru.tchallenge.service.complex.common.GenericWarmer
+import ru.tchallenge.service.complex.convention.component.WarmerComponent
+
+@CompileStatic
+@WarmerComponent
+class AccountWarmer extends GenericWarmer {
+
+    @Autowired
+    protected AccountService accountService
+
+    @Override
+    void run() {
+        accountService.search(new AccountSearchInvoice(
+                pageNumber: 1,
+                pageSize: 1000
+        ))
+    }
+}
