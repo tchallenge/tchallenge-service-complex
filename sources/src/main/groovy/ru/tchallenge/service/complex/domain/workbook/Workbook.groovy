20220d39b86dcede6eb9f564bf901be1b7c5d83c
onlythenaive <ilya.gubarev@gmail.com>
2017-07-02 19:26:28 +0300

    Workbook table name fixed

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/workbook/Workbook.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/workbook/Workbook.groovy
@@ -3,11 +3,14 @@ package ru.tchallenge.service.complex.domain.workbook
 import javax.persistence.Entity
 import javax.persistence.Table
 
+import groovy.transform.CompileStatic
+
 import ru.tchallenge.service.complex.common.ordinal.GenericOrdinalEntity
 import ru.tchallenge.service.complex.common.timestamp.TimestampedEntity
 
+@CompileStatic
 @Entity
-@Table(name = "workbooks")
+@Table(name = "workbook")
 class Workbook extends GenericOrdinalEntity implements TimestampedEntity {
 
 }
