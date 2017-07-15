644a3f8276ab29af9e0324d3329f0e99237b2d85
onlythenaive <ilya.gubarev@gmail.com>
2017-07-15 15:30:49 +0300

    logging mechanism improved

--- sources/src/main/groovy/ru/tchallenge/service/complex/common/GenericComponent.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/common/GenericComponent.groovy
@@ -21,25 +21,51 @@ abstract class GenericComponent {
     }
 
     void log(LogLevel level, String message, Object payload, Throwable throwable) {
-        logService.log(new LogRecord(
-                descriptor: throwable ? throwable.stackTrace[0].className : this.class.name,
+        def descriptor = throwable ? throwable.stackTrace[0].className : this.class.name
+        log(descriptor, level, message, payload, throwable)
+    }
+
+    void log(String descriptor, LogLevel level, String message, Object payload, Throwable throwable) {
+        def record = new LogRecord(
+                descriptor: descriptor,
                 level: level,
                 message: message,
                 payload: payload,
                 throwable: throwable
-        ))
+        )
+        logService.log(record)
+    }
+
+    void logAsTrace(String message) {
+        logAsTrace(message, null)
     }
 
     void logAsTrace(String message, Object payload) {
         log(LogLevel.TRACE, message, payload)
     }
 
+    void logAsTraceCaughtThrowable(Throwable throwable) {
+        log(this.class.name, LogLevel.TRACE, throwable.message, null, throwable)
+    }
+
+    void logAsDebug(String message) {
+        logAsDebug(message, null)
+    }
+
     void logAsDebug(String message, Object payload) {
         log(LogLevel.DEBUG, message, payload)
     }
 
+    void logAsInfo(String message) {
+        logAsInfo(message, null)
+    }
+
     void logAsInfo(String message, Object payload) {
         log(LogLevel.INFO, message, payload)
+    }
+
+    void logAsWarn(String message) {
+        logAsWarn(message, null)
     }
 
     void logAsWarn(String message, Object payload) {
--- sources/src/main/groovy/ru/tchallenge/service/complex/reliability/logging/LogServiceBean.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/reliability/logging/LogServiceBean.groovy
@@ -7,16 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.logging.LogLevel
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
 
 import ru.tchallenge.service.complex.common.GenericService
 import ru.tchallenge.service.complex.convention.component.ServiceComponent
 import ru.tchallenge.service.complex.reliability.correlation.CorrelationContext
 import ru.tchallenge.service.complex.reliability.correlation.CorrelationInfo
+import ru.tchallenge.service.complex.security.authentication.AuthenticationContext
+import ru.tchallenge.service.complex.security.authentication.AuthenticationInfo
 
 @CompileStatic
 @ServiceComponent
 class LogServiceBean extends GenericService implements LogService {
+
+    @Autowired
+    AuthenticationContext authenticationContext
 
     @Autowired
     CorrelationContext correlationContext
@@ -26,6 +32,14 @@ class LogServiceBean extends GenericService implements LogService {
 
     @Override
     void log(LogRecord record) {
+        try {
+            logRecord(record)
+        } catch (Throwable throwable) {
+            ownLog.fatal("Logging failed", throwable)
+        }
+    }
+
+    private void logRecord(LogRecord record) {
         def log = LogFactory.getLog(record.descriptor)
         def level = record.level
         def message = record.message
@@ -47,26 +61,52 @@ class LogServiceBean extends GenericService implements LogService {
     }
 
     private String serialize(String message, Object payload) {
+        def result = new StringBuilder(message)
+        if (payload) {
+            def payloadSerialized = objectMapper.writeValueAsString(payload)
+            result.append(": $payloadSerialized")
+        }
+        def attributes = attributes()
+        if (!attributes.empty) {
+            def joinedAttributes = attributes.join(", ")
+            result.append(" ($joinedAttributes)")
+        }
+        return result
+    }
+
+    private Collection<String> attributes() {
+        def result = []
         def correlation = correlationIfAny()
-        def correlationIdSerializedBlock = correlation ? "$correlation.id | " : ""
-        def correlationSerialized = json(correlation)
-        def correlationSerializedBlock = correlation ? ", (correlation: $correlationSerialized)" : ""
-        def payloadSerialized = json(payload)
-        def payloadSerializedBlock = payload ? ": $payloadSerialized" : ""
-        return correlationIdSerializedBlock + message + payloadSerializedBlock + correlationSerializedBlock
+        if (correlation) {
+            result.add("correlation: $correlation.id")
+        }
+        def authentication = authenticationIfAny()
+        if (authentication) {
+            result.add("account: $authentication.account.id")
+            result.add("logon: $authentication.token.createdAt")
+        }
+        return result
+    }
+
+    private AuthenticationInfo authenticationIfAny() {
+        try {
+            return authenticationContext.authentication
+        } catch (BeanCreationException exception) {
+            ownLog.trace("No authentication is available", exception)
+            return null
+        }
     }
 
     private CorrelationInfo correlationIfAny() {
         try {
             return correlationContext.correlation.orElse(null)
         } catch (BeanCreationException exception) {
-            def log = LogFactory.getLog(this.class.name)
-            log.trace("No correlation is available ($exception.message)")
+            ownLog.trace("No correlation is available", exception)
             return null
         }
     }
 
-    private String json(Object object) {
-        return object ? objectMapper.writeValueAsString(object) : null
+    private Log getOwnLog() {
+        LogFactory.getLog(this.class.name)
     }
 }
