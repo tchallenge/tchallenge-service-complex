dac81728e41c4e7faaece927714e1a5cf52dd25b
onlythenaive <ilya.gubarev@gmail.com>
2017-07-15 17:35:30 +0300

    Serialization configurator added, web configurator simplified, code quality improved.

---
+++ sources/src/main/groovy/ru/tchallenge/service/complex/SerializationConfigurator.groovy
@@ -0,0 +1,39 @@
+package ru.tchallenge.service.complex
+
+import groovy.transform.CompileStatic
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+
+import ru.tchallenge.service.complex.utility.serialization.InstantDeserializer
+import ru.tchallenge.service.complex.utility.serialization.InstantSerializer
+
+@CompileStatic
+@Configuration
+class SerializationConfigurator {
+
+    @Bean
+    ObjectMapper jacksonObjectMapper() {
+        new Jackson2ObjectMapperBuilder()
+                .failOnEmptyBeans(false)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+                .serializers(instantSerializer())
+                .deserializers(instantDeserializer())
+                .build()
+    }
+
+    @Bean
+    InstantSerializer instantSerializer() {
+        new InstantSerializer()
+    }
+
+    @Bean
+    InstantDeserializer instantDeserializer() {
+        new InstantDeserializer()
+    }
+}
