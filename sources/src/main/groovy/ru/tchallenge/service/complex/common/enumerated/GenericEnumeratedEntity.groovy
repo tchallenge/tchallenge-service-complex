5aa4daf05fa880c9bf73a9fc5af0febd6959ea0a
onlythenaive <ilya.gubarev@gmail.com>
2017-07-22 01:37:48 +0300

    Enumerated entity improved

--- sources/src/main/groovy/ru/tchallenge/service/complex/common/enumerated/GenericEnumeratedEntity.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/common/enumerated/GenericEnumeratedEntity.groovy
@@ -14,6 +14,7 @@ import ru.tchallenge.service.complex.common.GenericEntity
 @MappedSuperclass
 abstract class GenericEnumeratedEntity extends GenericEntity<String> implements Comparable<GenericEnumeratedEntity> {
 
+    @NotNull
     @Id
     @Column(name = 'id', nullable = false, unique = true, updatable = false)
     String id
