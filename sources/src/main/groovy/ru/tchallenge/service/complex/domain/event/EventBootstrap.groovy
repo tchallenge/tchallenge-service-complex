f73d053364f3a30d9ecd78b710e6baf7e4d785d7
onlythenaive <ilya.gubarev@gmail.com>
2017-07-24 16:18:50 +0300

    unused imports removed

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventBootstrap.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventBootstrap.groovy
@@ -1,18 +1,8 @@
 package ru.tchallenge.service.complex.domain.event
 
 import groovy.transform.CompileStatic
-import groovy.transform.PackageScope
-
-import org.springframework.beans.factory.annotation.Autowired
 
 import ru.tchallenge.service.complex.common.ordinal.GenericOrdinalBootstrap
-import ru.tchallenge.service.complex.common.ordinal.GenericOrdinalBootstrapBean
-import ru.tchallenge.service.complex.convention.component.BootstrapComponent
-import ru.tchallenge.service.complex.domain.event.category.EventCategory
-import ru.tchallenge.service.complex.domain.event.category.EventCategoryRepository
-import ru.tchallenge.service.complex.domain.event.interval.EventInterval
-import ru.tchallenge.service.complex.domain.event.status.EventStatus
-import ru.tchallenge.service.complex.domain.event.status.EventStatusRepository
 
 @CompileStatic
 interface EventBootstrap extends GenericOrdinalBootstrap<Event> {
