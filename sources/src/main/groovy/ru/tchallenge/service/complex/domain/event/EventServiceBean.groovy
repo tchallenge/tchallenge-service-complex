103cea4042f49204c171fe2850e45ec10f97882c
onlythenaive <ilya.gubarev@gmail.com>
2017-07-25 17:06:17 +0300

    EventService validation improved

--- sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventServiceBean.groovy
+++ sources/src/main/groovy/ru/tchallenge/service/complex/domain/event/EventServiceBean.groovy
@@ -9,6 +9,11 @@ import ru.tchallenge.service.complex.common.GenericServiceBean
 import ru.tchallenge.service.complex.common.enumerated.EnumeratedInvoice
 import ru.tchallenge.service.complex.common.search.SearchInfo
 import ru.tchallenge.service.complex.convention.component.ServiceComponent
+import ru.tchallenge.service.complex.reliability.exception.ContractViolationException
+import ru.tchallenge.service.complex.reliability.exception.ResourceViolationException
+import ru.tchallenge.service.complex.reliability.exception.ViolationException
+import ru.tchallenge.service.complex.reliability.violation.ContractViolationInfo
+import ru.tchallenge.service.complex.reliability.violation.ResourceViolationInfo
 
 @CompileStatic
 @PackageScope
@@ -26,18 +31,69 @@ class EventServiceBean extends GenericServiceBean implements EventService {
 
     @Override
     EventInfo create(EventInvoice invoice) {
-        def $event = eventMapper.asEntity(invoice.with {
+
+        // NOTE: textcode validation
+        def $textcode = invoice.textcode
+        if (!$textcode) {
+            throw missing('textcode')
+        }
+        def $min = 1
+        def $max = 16
+        if ($textcode.length() < $min || $textcode.length() > $max) {
+            throw illegalLength('textcode', $textcode, $min, $max)
+        }
+        def $alphanumericPattern = '.*'
+        if (!matches($textcode, $alphanumericPattern)) {
+            throw notMatchPatternOfAlphanumeric('textcode', $textcode)
+        }
+
+        // NOTE: title validation
+        if (!invoice.title) {
+            throw missing('title')
+        }
+        if ($textcode.length() < 1 || $textcode.length() > 16) {
+            throw missing('textcode')
+        }
+
+        // other checks connected to title (f.e. length)...
+        if (!invoice.greeting) {
+            throw missing('greeting')
+        }
+        // other checks connected to greeting (f.e. length)...
+        if (!invoice.intervals) {
+            throw missing('intervals')
+        }
+        if (!invoice.intervals.empty) {
+            throw missing('intervals')
+        }
+        def $intervalIndex = 0
+        for (def $interval : invoice.intervals) {
+            if (!$interval.since) {
+                throw missing("intervals[${$intervalIndex}].since")
+            }
+            if (!$interval.until) {
+                throw missing("intervals[${$intervalIndex}].until")
+            }
+            if ($interval.since >= $interval.until) {
+                throw missing("intervals[${$intervalIndex}].until")
+            }
+        }
+        def $normalizedInvoice = invoice.with {
             id = null
             status = initialStatus()
             it
-        })
+        }
+        def $event = eventMapper.asEntity($normalizedInvoice)
+        // TODO: implement pre-persist validation
+        def $result = saveAndInfo($event)
         logAsInfo('New event has been created', $event)
-        saveAndInfo($event)
+        $result
     }
 
     @Override
     EventInfo getByTextcode(String textcode) {
-        info(eventByTextcode(textcode))
+        def $event = eventByTextcode(textcode)
+        info($event)
     }
 
     @Override
@@ -56,11 +112,12 @@ class EventServiceBean extends GenericServiceBean implements EventService {
     @Override
     EventInfo update(EventInvoice invoice) {
         def $event = eventById(invoice.id)
-        def $trimmedInvoice = invoice.with {
+        def $normalizedInvoice = invoice.with {
             id = null
             it
         }
-        def $updatedEvent = eventMapper.asEntity($event, $trimmedInvoice)
+        def $updatedEvent = eventMapper.asEntity($event, $normalizedInvoice)
+        // TODO: implement pre-update validation
         def $result = saveAndInfo($updatedEvent)
         logAsInfo('Event has been updated', $result)
         $result
@@ -69,7 +126,7 @@ class EventServiceBean extends GenericServiceBean implements EventService {
     private Event eventById(String id) {
         def $result = eventRepository.findById(id as Long)
         if (!$result) {
-            throw unknownEvent()
+            throw eventNotFound('id', id)
         }
         $result
     }
@@ -77,7 +134,7 @@ class EventServiceBean extends GenericServiceBean implements EventService {
     private Event eventByTextcode(String textcode) {
         def $result = eventRepository.findByTextcode(textcode)
         if (!$result) {
-            throw unknownEvent()
+            throw eventNotFound('textcode', textcode)
         }
         $result
     }
@@ -94,11 +151,37 @@ class EventServiceBean extends GenericServiceBean implements EventService {
         info(save(event))
     }
 
-    private static EnumeratedInvoice initialStatus() {
-        enumerateds.invoice('CREATED')
+    private ViolationException missing(String path) {
+        def $violation = ContractViolationInfo.missing(path)
+        new ContractViolationException(this.class, $violation)
     }
 
-    private static RuntimeException unknownEvent() {
-        new RuntimeException('referenced event does not exist')
+    private ViolationException illegalLength(String path, Object value, int min, int max) {
+        throw new UnsupportedOperationException()
+    }
+
+    private ViolationException illegalNumberOfItems(String path, int number, int min, int max) {
+        throw new UnsupportedOperationException()
+    }
+
+    private ViolationException notMatchPattern(String path, Object value, String pattern) {
+        throw new UnsupportedOperationException()
+    }
+
+    private ViolationException notMatchPatternOfAlphanumeric(String path, Object value) {
+        throw new UnsupportedOperationException()
+    }
+
+    private boolean matches(String value, String pattern) {
+        throw new UnsupportedOperationException()
+    }
+
+    private ViolationException eventNotFound(String identifierName, Object identifierValue) {
+        def $violation = ResourceViolationInfo.notFound('event', identifierName, identifierValue)
+        new ResourceViolationException(this.class, $violation)
+    }
+
+    private static EnumeratedInvoice initialStatus() {
+        enumerateds.invoice('CREATED')
     }
 }
